005f1ba360a068d33d5315ec18a62a905586a10b
Ryan Alves <Ryan246.contato@gmail.com>
2024-07-29 23:04:59 -0300

    optmizing the code and adding functions.

--- Medical Insurance Project.py
+++ Medical Insurance Project.py
@@ -1,40 +1,13 @@
-# create the initial variables below
-age = 28
-sex = 0 # 0 female, 1 male
-bmi = float (26.2)
-num_of_children = 3
-smoker = 0 # 0 for non-smoker, 1 for a smoker
+# Create calculate_insurance_cost() function below: 
+def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name):
+  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+  print("The estimated insurance cost for " + str(name) + " is "  + str(estimated_cost) + " dollars.")
+  return estimated_cost
 
-# Add insurance estimate formula below
-insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+# Estimate Maria's insurance cost
+maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0, "Maria")
 
-print ("\n This person's insurance cost is " + str(insurance_cost) + " dollars.")
+# Estimate Omar's insurance cost 
+omar_insurance_cost = calculate_insurance_cost(35, 1, 22.2, 0, 1, "Omar")
 
-# Age Factor
-age += 4
-
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print ("\n The change in cost of insurance after increasing the age by 4 years is " + str(change_in_insurance_cost) + " dollars.")
-
-# BMI Factor
-age -= 4
-bmi += 3.1
-
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print ("\n The change in estimated insurance cost after increasing BMI by 3.1 is " + str(change_in_insurance_cost) + " dollars.")
-
-# Male vs. Female Factor
-bmi -= 3.1
-sex = 1 # 0 female, 1 male
-
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print ("\n The change in estimated cost for being male instead of female is " + str(change_in_insurance_cost) + " dollars.")
+ryan_insurance_cost = calculate_insurance_cost(20, 1, 20.2, 0, 0, "Ryan")
